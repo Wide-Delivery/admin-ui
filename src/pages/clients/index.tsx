e70119f3f4223661335ca6b44990ad8420da4c53
Anton Ovsiannikov <ovsiannikov@managed-code.com>
2024-06-09 23:27:49 +0300

    Add clients page

--- src/pages/clients/index.tsx
+++ src/pages/clients/index.tsx
@@ -1,5 +1,44 @@
+import { Avatar, Box, Grid, Typography } from "@mui/material";
+import PageTitle from "@/components/UI/PageTitle";
+import { mockClients } from "../../constants";
+
 const ClientsPage = () => {
-	return <div>ClientsPage</div>;
+	return (
+		<Box sx={{ display: "flex", flexDirection: "column", gap: 5 }}>
+			<PageTitle pageTitle={"Clietns page"} />
+			<Grid container spacing={4}>
+				{mockClients.map((client) => (
+					<Grid item xs={12} sm={6} md={4} lg={3} key={client._id.$oid}>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+								p: 2,
+								boxShadow: 3,
+								borderRadius: 2,
+							}}
+						>
+							<Avatar
+								alt={client.name}
+								src={client.photo}
+								sx={{ width: 100, height: 100, mb: 2 }}
+							/>
+							<Typography variant="h6" component="h2">
+								{client.name}
+							</Typography>
+							<Typography variant="body2" color="textSecondary">
+								Email: {client.email}
+							</Typography>
+							<Typography variant="body2" color="textSecondary">
+								Role: {client.role}
+							</Typography>
+						</Box>
+					</Grid>
+				))}
+			</Grid>
+		</Box>
+	);
 };
 
 export default ClientsPage;
